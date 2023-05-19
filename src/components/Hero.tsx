b48c7c755a4c5b68db7dde44ccc838233a2a7270
maziyar parsi <mparsi1992@gmail.com>
2023-05-19 19:56:25 +0330

    Fix: add intial three js

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../config/styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto ">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[32px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        {/* violet line */}
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        {/* text */}
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white my-2`}>
+            Hi, My name is <span className="text-[#915eff] flex">Maziyar</span>
+          </h1>
+          <p className={`${styles.heroSubText} text-white-100`}>
+            I'm a front-end developer with industry
+            <br /> experience building website and web applications
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
