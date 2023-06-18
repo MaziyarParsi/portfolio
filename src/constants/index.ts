cacd5af4e9a01388944123cf3780b0baaf7dd062
maziyar parsi <mparsi1992@gmail.com>
2023-06-18 16:49:06 +0330

    fix: recomendations

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -176,28 +176,29 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
-    name: 'Sara Lee',
-    designation: 'CFO',
-    company: 'Acme Co',
-    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+      'I highly recommend Maziyar as a software engineer. His extensive knowledge, dedication, and passion make him an invaluable asset. Maziyar excels in breaking down complex concepts',
+    name: 'Ehsan Gazar',
+    designation: 'Technical Team Lead',
+    company: 'Mecca',
+    image:
+      'https://media.licdn.com/dms/image/C5603AQHWuhLqZhJ1vA/profile-displayphoto-shrink_100_100/0/1659587216801?e=1692835200&v=beta&t=D7Zv3sLbbV27l56q4xywGSUXFKby8tHabzAgyN7QKdo',
   },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: 'Chris Brown',
-    designation: 'COO',
-    company: 'DEF Corp',
-    image: 'https://randomuser.me/api/portraits/men/5.jpg',
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: 'Lisa Wang',
-    designation: 'CTO',
-    company: '456 Enterprises',
-    image: 'https://randomuser.me/api/portraits/women/6.jpg',
-  },
+  // {
+  //   testimonial:
+  //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
+  //   name: 'Chris Brown',
+  //   designation: 'COO',
+  //   company: 'DEF Corp',
+  //   image: 'https://randomuser.me/api/portraits/men/5.jpg',
+  // },
+  // {
+  //   testimonial:
+  //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //   name: 'Lisa Wang',
+  //   designation: 'CTO',
+  //   company: '456 Enterprises',
+  //   image: 'https://randomuser.me/api/portraits/women/6.jpg',
+  // },
 ];
 
 const projects = [
